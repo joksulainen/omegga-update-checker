e34abf550f6a476f0c207b65a2d884f8d9f56568
joksulainen <54633559+joksulainen@users.noreply.github.com>
2025-07-24 22:56:23 +0300

    this might be functional now, needs testing

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -6,9 +6,38 @@ import fetch from 'node-fetch';
 type Config = {};
 type Storage = {};
 
+// plugin version here for convenience
+const PLUGIN_VERSION = '0.1.0';
+
+// types to help with type safety
+type PluginUpdateInfo = GHPluginUpdateInfo | GLPluginUpdateInfo;
+
+type GHPluginUpdateInfo = {
+  _check_count?: number
+  version: string
+  api_type: 'github'
+  repo_info: {
+    owner: string
+    repo: string
+  }
+};
+
+type GLPluginUpdateInfo = {
+  _check_count?: number
+  version: string
+  api_type: 'gitlab'
+  repo_info: {
+    project_id: string
+  }
+};
+
 // helper functions
 function semverIsGreater(a: string, b: string): boolean {
   return a.localeCompare(b, undefined, { numeric: true }) > 0;
+}
+
+function isPluginUpdateInfo(updateInfo: PluginUpdateInfo): updateInfo is PluginUpdateInfo {
+  return updateInfo.api_type === 'github' || updateInfo.api_type === 'gitlab';
 }
 
 export default class Plugin implements OmeggaPlugin<Config, Storage> {
@@ -16,7 +45,7 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   config: PC<Config>;
   store: PS<Storage>;
   
-  plugins: Set<string>;
+  plugins: Record<string, PluginUpdateInfo>;
   interval: NodeJS.Timeout;
   
   constructor(omegga: OL, config: PC<Config>, store: PS<Storage>) {
@@ -24,19 +53,46 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     this.config = config;
     this.store = store;
     
-    this.plugins = new Set();
+    this.plugins = {};
     
     this.updateCheckerCallback = this.updateCheckerCallback.bind(this);
+    this.checkUpdate = this.checkUpdate.bind(this);
+    this.pluginStatusCallback = this.pluginStatusCallback.bind(this);
   }
   
   async updateCheckerCallback() {
-    // fetch the latest release from github
-    const data = await (await fetch(REMOTE_URL, {
-      headers: {
-        'Accept': 'application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28',
-      },
-    })).json();
+    Object.entries(this.plugins).forEach(([name, info]) => {
+      this.checkUpdate(name, info);
+    });
+  }
+  
+  async checkUpdate(name: string, info: PluginUpdateInfo) {
+    let response: fetch.Response | void = undefined;
+    
+    // fetch the latest release from the respective platform
+    if (info.api_type === 'github') {
+      response = await fetch(`https://api.github.com/repos/${info.repo_info.owner}/${info.repo_info.repo}/releases/latest`, {
+        headers: {
+          'Accept': 'application/vnd.github+json',
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      }).catch(() => {
+        console.error(`Failed to fetch release data for \x1b[92m${name}\x1b[0m`);
+      });
+    }
+    else if (info.api_type === 'gitlab') {
+      response = await fetch(`https://gitlab.com/api/v4/projects/${info.repo_info.project_id}/releases/permalink/latest`, {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }).catch(() => {
+        console.error(`Failed to fetch release data for \x1b[92m${name}\x1b[0m`);
+      });
+    }
+    
+    // stop if there is no response
+    if (!response) return;
+    const data = await response.json();
     
     // we probably shouldnt continue if its a pre-release
     if (data['prerelease']) return;
@@ -46,33 +102,86 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
     if (!remoteVersionMatch) return;
     const remoteVersion = remoteVersionMatch[1];
     
-    // proceed if githubVersion is a string and is a greater semver than PLUGIN_VERSION
+    // proceed if remoteVersion is a string and is a greater semver than info.version
     if (typeof remoteVersion !== 'string') return;
-    if (!semverIsGreater(remoteVersion, PLUGIN_VERSION)) return;
+    if (!semverIsGreater(remoteVersion, info.version)) return;
     
-    // there is a newer version available on github, we should log it
-    this.omegga.broadcast(`[<color="#AAFFAA">rolelogger</>]: A new version is available: ${PLUGIN_VERSION} -> ${remoteVersion}`);
-    console.info(`A new version is available: ${PLUGIN_VERSION} -> ${remoteVersion}`);
+    // there is a newer version available on remote, we should log it
+    this.omegga.broadcast(`[<color="#AAFFAA">${name}</>]: A new version is available: ${info.version} -> ${remoteVersion}`);
+    console.info(`A new version for \x1b[92m${name}\x1b[0m is available: ${info.version} -> ${remoteVersion}`);
   }
   
-  async pluginEvent(event: string, from: string, ..._args: any[]) {
+  async pluginEvent(event: string, from: string, info?: PluginUpdateInfo) {
+    // a plugin wants to be checked for updates
+    if (event === 'hook') {
+      // check if the plugin has already hooked
+      if (from in this.plugins) {
+        console.log(`Plugin \x1b[92m${from}\x1b[0m has already hooked into update-checker`);
+        return;
+      }
+      
+      // do some data validation to make sure plugins dont provide garbage data
+      if (!info) {
+        console.error(`Plugin \x1b[92m${from}\x1b[0m did not provide update info to be hooked`);
+        return;
+      }
+      
+      const semverMatch = info.version.match(/^(?:\d+)\.(?:\d+)\.(?:\d+)$/);
+      if (!semverMatch) {
+        console.error(`Plugin \x1b[92m${from}\x1b[0m version isn't a valid semantic version`);
+        return;
+      }
+      
+      if (!isPluginUpdateInfo(info)) {
+        console.error(`Plugin \x1b[92m${from}\x1b[0m update info doesn't match type`);
+        return;
+      }
+      
+      // all validation has passed, add plugin to record
+      this.plugins[from] = info;
+      console.log(`Plugin \x1b[92m${from}\x1b[0m hooked into update-checker`);
+    }
+    // a plugin no longer wants to be checked for updates
+    else if (event === 'unhook') {
+      // check if the plugin isnt hooked
+      if (!(from in this.plugins)) {
+        console.log(`Plugin \x1b[92m${from}\x1b[0m isn't hooked into update-checker`);
+        return;
+      }
+      
+      // unhook plugin
+      delete this.plugins[from];
+      console.log(`Plugin \x1b[92m${from}\x1b[0m unhooked from update-checker`);
+    }
+  }
+  
+  // plugin hook clean up on plugin unload
+  async pluginStatusCallback(name: string, plugin: { name: string, isLoaded: boolean, isEnabled: boolean }) {
+    if (!(name in this.plugins) || plugin.isLoaded) return;
     
+    delete this.plugins[name];
+    console.log(`Plugin \x1b[92m${name}\x1b[0m was unloaded and unhooked from update-checker`);
   }
   
   async init() {
-    // Write your plugin!
-    this.omegga.on('cmd:test', (speaker: string) => {
-      this.omegga.broadcast(`Hello, ${speaker}!`);
-    });
+    // add this plugins update info
+    this.plugins['update-checker'] = {
+      version: PLUGIN_VERSION,
+      api_type: 'github',
+      repo_info: { owner: 'joksulainen', repo: 'omegga-update-checker' },
+    };
     
-    // emit event so plugins know that this plugin is ready to receive hooks
+    // notify other plugins that this plugin is ready to receive hooks
     this.omegga.emit('uc:ready');
+    
+    // add listener for hook clean up
+    this.omegga.on('plugin:status', this.pluginStatusCallback);
     
     // add an interval as well as trigger the callback after a delay to do a first check
     this.interval = setInterval(this.updateCheckerCallback, 14400000); // every 4 hours: 4*60*60*1000
     setTimeout(this.updateCheckerCallback, 1000);
     
-    return { registeredCommands: ['test'] };
+    return {};
   }
   
   async stop() {
