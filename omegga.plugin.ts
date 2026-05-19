d3b2112e20e85188685c5dad1fcc0239bbf7ee4a
joksulainen <54633559+joksulainen@users.noreply.github.com>
2026-05-19 20:04:01 +0300

    new file based update arch, plugin load order agnostic

--- omegga.plugin.ts
+++ omegga.plugin.ts
@@ -1,5 +1,6 @@
 import { OmeggaPlugin, OL, PS, PC } from 'omegga';
 import fetch, { Response } from 'node-fetch';
+import semver from 'semver';
 import fs from 'node:fs';
 
 
@@ -34,10 +35,6 @@ type GLPluginUpdateInfo = {
 };
 
 // helper functions
-function semverIsGreater(a: string, b: string): boolean {
-  return a.localeCompare(b, undefined, { numeric: true }) > 0;
-}
-
 function isPluginUpdateInfo(updateInfo: PluginUpdateInfo): updateInfo is PluginUpdateInfo {
   return updateInfo.api_type === 'github' || updateInfo.api_type === 'gitlab';
 }
@@ -54,98 +51,114 @@ export default class Plugin implements OmeggaPlugin<Config, Storage> {
   config: PC<Config>;
   store: PS<Storage>;
   
-  plugins: Record<string, PluginUpdateInfo>;
-  interval: NodeJS.Timeout;
+  interval: NodeJS.Timeout | undefined;
   
   constructor(omegga: OL, config: PC<Config>, store: PS<Storage>) {
     this.omegga = omegga;
     this.config = config;
     this.store = store;
     
-    this.plugins = {};
-    
     this.updateCheckerCallback = this.updateCheckerCallback.bind(this);
     this.checkUpdate = this.checkUpdate.bind(this);
   }
   
   async updateCheckerCallback() {
+    // get all plugins in the plugins directory
+    const plugins = fs.readdirSync('./plugins/');
+    const promises = Array<Promise<{ name: string, local_ver: string, remote_ver: string } | undefined>>();
+    
     // perform update checks and clean up any stale hooks while at it
-    for (const [pName, uInfo] of Object.entries(this.plugins)) {
-      // check if its a stale hook and clean it up
-      const plugin = await this.omegga.getPlugin(pName);
-      if (!plugin || !plugin.loaded) {
-        delete this.plugins[pName];
-        console.warn(`Removed stale hook for ${ansiWrapper(PLUGIN_ANSI, pName)}`);
+    for (const plugin of plugins) {
+      // is the plugin ignored in the config?
+      if (this.config.ignored_plugins.includes(plugin)) continue;
+      
+      // check if the plugin has a uc-info.json file and load it, otherwise the plugin does not use this plugin
+      if (!fs.existsSync(`./plugins/${plugin}/uc-info.json`)) continue;
+      
+      const uInfo = JSON.parse(fs.readFileSync(`./plugins/${plugin}/uc-info.json`, 'utf-8').toString());
+      if (!isPluginUpdateInfo(uInfo)) { // check if the json conforms to the PluginUpdateInfo format
+        console.warn(`${plugin} uc-info.json is malformed, skipping`);
+        continue;
+      }
+      if (!semver.valid(uInfo.version)) {
+        console.warn(`${plugin} version in uc-info.json is not a semver, skipping`);
         continue;
       }
       
-      // is the plugin ignored in the config?
-      if (pName in this.config.ignored_plugins) continue;
-      
       // perform the update check
-      this.checkUpdate(pName, uInfo);
+      promises.push(this.checkUpdate(plugin, uInfo));
+    }
+    
+    const results = await Promise.allSettled(promises);
+    
+    const resultString = results.flatMap(r => r.status === 'fulfilled' && r.value ? [r.value] : [])
+      .map(u => `${ansiWrapper(PLUGIN_ANSI, u.name)} ${u.local_ver} -> ${u.remote_ver}`).join(', ');
+    
+    if (resultString.length > 0) {
+      if (this.config.notify_in_chat) {
+        this.omegga.broadcast('<code><color="#AAFFAA">update-checker</></>: There are available updates for plugins. Check server console for details.');
+      }
+      console.info(`Available plugin updates: ${resultString}`);
+    }
+    else {
+      console.info('No available plugin updates');
     }
   }
   
   async checkUpdate(name: string, info: PluginUpdateInfo) {
-    let response: Response | void = undefined;
-    
     console.info(`Checking for updates to ${ansiWrapper(PLUGIN_ANSI, name)}`);
     
     // fetch the latest release from the respective platform
+    let response: Response | void = undefined;
     if (info.api_type === 'github') {
       response = await fetch(`https://api.github.com/repos/${info.repo_info.owner}/${info.repo_info.repo}/releases/latest`, {
-        headers: {
-          'Accept': 'application/vnd.github+json',
-          'X-GitHub-Api-Version': '2022-11-28',
-        },
-      }).catch(() => {
-        console.warn(`Failed to fetch release data for ${ansiWrapper(PLUGIN_ANSI, name)}`);
+        headers: { 'Accept': 'application/vnd.github+json', 'X-GitHub-Api-Version': '2026-03-10' },
+      }).catch((e) => {
+        console.error(`Network error checking ${ansiWrapper(PLUGIN_ANSI, name)}:`, e);
+        throw e;
       });
     }
     else if (info.api_type === 'gitlab') {
-      response = await fetch(`https://gitlab.com/api/v4/projects/${info.repo_info.project_id}/releases/permalink/latest`, {
-        headers: {
-          'Content-Type': 'application/json',
-        },
-      }).catch(() => {
-        console.warn(`Failed to fetch release data for ${ansiWrapper(PLUGIN_ANSI, name)}`);
+      response = await fetch(`https://gitlab.com/api/v4/projects/${info.repo_info.project_id}/releases/`, {
+        headers: { 'Content-Type': 'application/json' },
+      }).catch((e) => {
+        console.error(`Network error checking ${ansiWrapper(PLUGIN_ANSI, name)}:`, e);
+        throw e;
       });
     }
     
     // stop if there is no response
     if (!response || !response.ok) {
       console.warn(`Failed to fetch release data for ${ansiWrapper(PLUGIN_ANSI, name)}`);
-      return;
+      throw new Error(`Failed to fetch release data for ${name}: ${response?.status} ${response?.statusText}`);
     }
     const data = await response.json();
     
-    // we probably shouldnt continue if its a pre-release
-    if (data['prerelease'] || data['upcoming_release']) {
-      console.info(`No updates available for ${ansiWrapper(PLUGIN_ANSI, name)}`);
-      return;
+    // filter data based on api type
+    let remoteVersion: string | null = null;
+    if (info.api_type === 'github') {
+      // see if the tag name contains a stable semver and grab it
+      remoteVersion = semver.clean(data.tag_name);
+    }
+    else if (info.api_type === 'gitlab') {
+      for (const release of data) {
+        // we shouldnt use an upcoming release
+        if (release.upcoming_release) continue;
+        
+        // see if the tag name contains a stable semver and grab it
+        remoteVersion = semver.clean(release.tag_name);
+        break;
+      }
     }
     
-    // see if the tag name contains a stable semver and grab it, otherwise end early
-    const remoteVersionMatch = ((data['tag_name'] as string).match(/^.*((?:\d+)\.(?:\d+)\.(?:\d+))$/));
-    if (!remoteVersionMatch) {
-      console.info(`No updates available for ${ansiWrapper(PLUGIN_ANSI, name)}`);
-      return;
-    }
-    const remoteVersion = remoteVersionMatch[1];
+    // end if the semver is invalid
+    if (!remoteVersion) return;
     
     // proceed if remoteVersion is a greater semver than info.version
-    if (!semverIsGreater(remoteVersion, info.version)) {
-      console.info(`No updates available for ${ansiWrapper(PLUGIN_ANSI, name)}`);
-      return;
-    }
+    if (!semver.gt(remoteVersion, info.version)) return;
     
-    // there is a newer version available on remote, we should log it
-    if (this.config.notify_in_chat) {
-      this.omegga.broadcast(`<code><color="#AAFFAA">${name}</></>: A new version is available: ${info.version} -> ${remoteVersion}`);
-    }
-    
-    console.info(`A new version of ${ansiWrapper(PLUGIN_ANSI, name)} is available: ${info.version} -> ${remoteVersion}`);
+    // a newer version is available
+    return { name: name, local_ver: info.version, remote_ver: remoteVersion };
   }
   
   async init() {
